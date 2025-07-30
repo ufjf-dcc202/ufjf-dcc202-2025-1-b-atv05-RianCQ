fb9323bd9ef2c9a57e7a4447663640f1801a1ac8
Rian Cesar Quintanilha <rianquintanilha5@gmail.com>
2025-07-30 17:21:44 -0300

    importa a função getTabuleiro do arquivo discos.js

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
+import { getTabuleiro } from "./discos.js";
+
 const eH1 = document.querySelector("h1");
 eH1.textContent = "Olá, Mundo!";
 
 const eTabuleiro = criaTabuleiro();
 document.body.append(eTabuleiro);
 
+const tabuleiro = getTabuleiro();
 for(let i=0; i<7; i++){
     const eDisco = criaDisco();
     eTabuleiro.append(eDisco);
-    if(i<3){
+    /*if(i<3){
         eDisco.dataset.cor = 'branco';
     } else if(i>3){
         eDisco.dataset.cor = 'preto';
     }
     else {
         eDisco.dataset.cor = 'transparente';
-    }
+    }*/
+   eDisco.dataset.cor = tabuleiro[i];
 }
 function criaDisco(){
     const novoDisco = document.createElement("div");
